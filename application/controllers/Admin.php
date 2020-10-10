501ecf3cd394405c4fe6415537311ba4a81c0c7f
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2020-10-10 10:51:52 +0700

    Add dashboard template

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -8,6 +8,25 @@ class Admin extends CI_Controller {
 		if(!$this->session->userdata('username')) {
 			redirect('/');
 		}
+
+		$user = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+		$role = $this->session->userdata('role');
+		$menu = $this->AdminModel->dashboard($role);
+
+		$data = [
+			'title'		=> 'Dashboard | Sistem Pengelola Inventaris',
+			'menu'		=> 'dashboard',
+			'submenu'	=> 'dashboard',
+			'user'		=> $user,
+			'menulist'	=> $menu,
+			'db'		=> $this->load->database(),
+			
+		];
+
+		$this->load->view('template/header', $data);
+		$this->load->view('template/sidebar', $data);
+		$this->load->view('admin/dashboard', $data);
+		$this->load->view('template/footer');
 	}
 
 }
